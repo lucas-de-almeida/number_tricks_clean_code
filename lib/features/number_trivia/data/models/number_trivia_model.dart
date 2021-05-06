8893340e11e389437cc1386e725f35a3c9775b2a
Almeida <lalmeida6@PMINTL.NET>
2021-05-06 04:46:36 -0300

    adicionada camada de model metodos to/from Json

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,25 @@
+import 'package:trivial_number/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  NumberTriviaModel({
+    String text,
+    int number,
+  }) : super(
+          text: text,
+          number: number,
+        );
+
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+      text: json['text'],
+      number: (json['number'] as num).toInt(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'text': text,
+      'number': number,
+    };
+  }
+}
